eddadfa035e83d54ba8dc757e0f14f487eee464a
Anamul-Haque <mdanamulhaquestatistics@gmail.com>
2024-12-09 18:15:41 +0600

    2nd

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 //Create a variable called carName, assign the value Volvo to it.
 
 let carName = "Volvo";
+
+
+//Use comments to describe the correct data type of the following variables:
+
+let firstName = "John", lastName = "Doe", age = 35;
+
+
